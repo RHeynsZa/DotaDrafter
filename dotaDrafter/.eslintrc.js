50b3e37f8259a99e0decb1c92458a95f7e72032f
RuanHeyns@Home <ruan@quantsolutions.co.za>
2021-05-16 11:56:32 +0200

    Add rxjs linting

--- dotaDrafter/.eslintrc.js
+++ dotaDrafter/.eslintrc.js
@@ -1,28 +1,31 @@
-{
-    "parser": "@typescript-eslint/parser", // Specifies the ESLint parser
-    "extends": [
+const { join } = require("path");
+module.exports = {
+    parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+    extends: [
         "plugin:angular/johnpapa", //ESLint rules for your angular project with checks for best-practices, conventions or potential errors.
         "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        "plugin:rxjs/recommended",
         "prettier"
     ],
-    "parserOptions": {
-        "ecmaVersion": 2020, // Allows for the parsing of modern ECMAScript features
-        "sourceType": "module" // Allows for the use of imports
+    parserOptions: {
+        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+        project: join(__dirname, "./tsconfig.json"),
+        sourceType: "module" // Allows for the use of imports
     },
-    "settings": {
-        "angular": {
+    settings: {
+        angular: {
             "version": "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
         }
     },
-    "root": true,
-    "env": {
-        "node": true,
-        "jest": true
+    root: true,
+    env: {
+        node: true,
+        jest: true
     },
-    "rules": {
+    rules: {
         "@typescript-eslint/interface-name-prefix": "off",
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/no-explicit-any": "error"
     },
-    "ignorePatterns": ["/*.*"]
+    ignorePatterns: ["/*.*"]
 }
